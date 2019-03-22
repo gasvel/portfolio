ff7f3690d78caab2e008bebbb281ed962a6e228f
gasvel <gastonveliez95@gmail.com>
2019-03-22 19:29:55 -0300

    Agregado bootstrap y elementos iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"",component: InicioComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
